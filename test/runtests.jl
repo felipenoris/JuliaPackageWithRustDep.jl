7549f1ea924fc832e766bd8a97aa323008c1613b
Felipe Noronha <felipenoris@gmail.com>
2018-09-24 20:01:05 -0300

    use Test

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,24 +6,24 @@ using Test
 JuliaPackageWithRustDep.rustdylib_printhello()
 
 # Int32
-println( JuliaPackageWithRustDep.rustdylib_abs_i32(Int32(10)) )
-println( JuliaPackageWithRustDep.rustdylib_abs_i32(Int32(-10)) )
+@test JuliaPackageWithRustDep.rustdylib_abs_i32(Int32(10)) == 10
+@test JuliaPackageWithRustDep.rustdylib_abs_i32(Int32(-10)) == 10
 
 # Int64
-println( JuliaPackageWithRustDep.rustdylib_abs_i64(typemax(Int64)) )
-println( JuliaPackageWithRustDep.rustdylib_abs_i64(typemin(Int64)) )
+@test JuliaPackageWithRustDep.rustdylib_abs_i64(typemax(Int64)) == typemax(Int64)
+@test JuliaPackageWithRustDep.rustdylib_abs_i64(typemin(Int64)) == typemax(Int64)
 
 # Float32
-println( JuliaPackageWithRustDep.rustdylib_abs_f32(Float32(1.0)) )
-println( JuliaPackageWithRustDep.rustdylib_abs_f32(Float32(-1.0)) )
+@test JuliaPackageWithRustDep.rustdylib_abs_f32(Float32(1.0)) == 1.0
+@test JuliaPackageWithRustDep.rustdylib_abs_f32(Float32(-1.0)) == 1.0
 
 # Float64
-println( JuliaPackageWithRustDep.rustdylib_abs_f64(Float64(1.0)) )
-println( JuliaPackageWithRustDep.rustdylib_abs_f64(Float64(-1.0)) )
+@test JuliaPackageWithRustDep.rustdylib_abs_f64(Float64(1.0)) == 1.0
+@test JuliaPackageWithRustDep.rustdylib_abs_f64(Float64(-1.0)) == 1.0
 
 # Bool
-println( JuliaPackageWithRustDep.rustdylib_is_true_bool(true) )
-println( JuliaPackageWithRustDep.rustdylib_is_true_bool(false) )
+@test JuliaPackageWithRustDep.rustdylib_is_true_bool(true) == true
+@test JuliaPackageWithRustDep.rustdylib_is_true_bool(false) == false
 
 JuliaPackageWithRustDep.rustdylib_inspect_string("hey you")
 JuliaPackageWithRustDep.rustdylib_inspect_string("é∈δ")
