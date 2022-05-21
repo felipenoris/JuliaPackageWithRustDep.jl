e752c0eae05de58ebc3e9ca5ccfa5b630720ccb2
Felipe Noronha <felipenoris@gmail.com>
2022-05-21 11:25:57 -0300

    bump rust 2021 edition. bump cbindgen v0.23.

--- deps/RustDylib/src/lib.rs
+++ deps/RustDylib/src/lib.rs
@@ -1,14 +1,13 @@
-
-use std::ffi::{CString, CStr};
+use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 
 #[no_mangle]
-pub extern fn rustdylib_printhello() {
+pub extern "C" fn rustdylib_printhello() {
     println!("Hello from Rust!");
 }
 
 #[no_mangle]
-pub extern fn rustdylib_abs_i32(i: i32) -> i32 {
+pub extern "C" fn rustdylib_abs_i32(i: i32) -> i32 {
     println!("Rust read i32 `{:?}`.", i);
     if i >= 0 {
         i
@@ -18,7 +17,7 @@ pub extern fn rustdylib_abs_i32(i: i32) -> i32 {
 }
 
 #[no_mangle]
-pub extern fn rustdylib_abs_i64(i: i64) -> i64 {
+pub extern "C" fn rustdylib_abs_i64(i: i64) -> i64 {
     println!("Rust read i64 `{:?}`.", i);
     if i >= 0 {
         i
@@ -28,7 +27,7 @@ pub extern fn rustdylib_abs_i64(i: i64) -> i64 {
 }
 
 #[no_mangle]
-pub extern fn rustdylib_abs_f32(i: f32) -> f32 {
+pub extern "C" fn rustdylib_abs_f32(i: f32) -> f32 {
     println!("Rust read f32 `{:?}`.", i);
     if i >= 0.0 {
         i
@@ -38,7 +37,7 @@ pub extern fn rustdylib_abs_f32(i: f32) -> f32 {
 }
 
 #[no_mangle]
-pub extern fn rustdylib_abs_f64(i: f64) -> f64 {
+pub extern "C" fn rustdylib_abs_f64(i: f64) -> f64 {
     println!("Rust read f64 `{:?}`.", i);
     if i >= 0.0 {
         i
@@ -48,14 +47,14 @@ pub extern fn rustdylib_abs_f64(i: f64) -> f64 {
 }
 
 #[no_mangle]
-pub extern fn rustdylib_is_true_bool(b: bool) -> bool {
+pub extern "C" fn rustdylib_is_true_bool(b: bool) -> bool {
     println!("Rust read bool `{:?}`.", b);
     b
 }
 
 #[no_mangle]
-pub extern fn rustdylib_inspect_string(cstring: *const c_char) {
-    let cstr = unsafe { CStr::from_ptr(cstring) };
+pub unsafe extern "C" fn rustdylib_inspect_string(cstring: *const c_char) {
+    let cstr = CStr::from_ptr(cstring);
 
     match cstr.to_str() {
         Ok(s) => {
@@ -69,16 +68,15 @@ pub extern fn rustdylib_inspect_string(cstring: *const c_char) {
 }
 
 #[no_mangle]
-pub extern fn rustdylib_generate_rust_owned_string() -> *mut c_char {
+pub extern "C" fn rustdylib_generate_rust_owned_string() -> *mut c_char {
     let rust_string = String::from("The bomb: 💣");
     let cstring = CString::new(rust_string).unwrap();
     cstring.into_raw() // transfers ownership to the Julia process
 }
 
 #[no_mangle]
-pub extern fn rustdylib_free_rust_owned_string(s: *mut c_char) {
-    unsafe {
-        if s.is_null() { return }
-        CString::from_raw(s) // retakes ownership of the CString
-    };
+pub unsafe extern "C" fn rustdylib_free_rust_owned_string(s: *mut c_char) {
+    if !s.is_null() {
+        drop(CString::from_raw(s)); // retakes ownership of the CString and drops
+    }
 }
